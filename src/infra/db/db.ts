899a4c10a6a198037713ff3f8f0ad9613a4f3c17
Michael Medvedev <mmedvedev20@gmail.com>
2026-03-10 18:53:05 -0700

    setting up drizzle orm

---
+++ src/infra/db/db.ts
@@ -0,0 +1,6 @@
+import { env } from "@/configs/env";
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+
+const client = postgres(env.DATABASE_URL, { prepare: false });
+export const db = drizzle({ client });
