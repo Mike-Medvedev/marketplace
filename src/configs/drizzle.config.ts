aad96167130cd5a2950e100057b40d346bb2317d
Michael <michael.medvedev@pwc.com>
2026-03-11 19:57:07 -0400

    migrated tabls

--- src/configs/drizzle.config.ts
+++ src/configs/drizzle.config.ts
@@ -1,12 +1,11 @@
 import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
-import { env } from "@/configs/env";
 
 export default defineConfig({
   out: "drizzle",
   schema: "src/infra/db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: env.DATABASE_URL!,
+    url: process.env.DATABASE_URL!,
   },
 });
