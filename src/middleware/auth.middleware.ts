2c3d978f6f7baad213dea541e0b04a2e0a71293b
Mike-Medvedev <mmedvedev20@gmail.com>
2026-03-11 20:00:30 -0400

    add handle SSE auth req

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -3,14 +3,16 @@ import { verifyUser } from "@/infra/auth/auth.service";
 import { MissingTokenError } from "@/shared/errors/errors";
 export async function validateUser(req: Request, _res: Response, next: NextFunction) {
   const authHeader = req.headers.authorization;
+  let jwt: string | undefined;
 
-  if (!authHeader?.startsWith("Bearer ")) {
-    return next(new MissingTokenError("Missing or invalid Authorization header"));
+  if (authHeader?.startsWith("Bearer ")) {
+    jwt = authHeader.split(" ")[1];
+  } else if (typeof req.query.token === "string") {
+    jwt = req.query.token;
   }
 
-  const jwt = authHeader.split(" ")[1];
   if (!jwt) {
-    return next(new MissingTokenError("Token missing from Authorization header"));
+    return next(new MissingTokenError("Missing or invalid Authorization header"));
   }
 
   const user = await verifyUser(jwt);
